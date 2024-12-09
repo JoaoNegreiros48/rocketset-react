0d0523dca2272c937d181c6adb783df7eed0ba69
João Negreiros <joaonegreiros48@gmail.com>
2024-12-09 20:58:01 -0300

    global style

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,17 +1,13 @@
-import {Post} from './Post'
+import {Header} from './components/Header'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="João Negreiros" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo unde voluptates doloremque optio, praesentium deserunt obcaecati iste vitae! Delectus numquam sequi corporis quis! Quam, dolorum. Dolore provident omnis unde officia!" 
-      />
-      
-      <Post 
-        author="Ronaldinho gaucho" 
-        content="Tão deixando a gente sonhar" 
-      />
+
+      <Header />
+
     </div>
   )
 }
