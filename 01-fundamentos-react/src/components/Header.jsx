abb6dd8fd0881de354b9deb584a25bae7dbe5f8d
João Negreiros <joaonegreiros48@gmail.com>
2024-12-09 21:07:58 -0300

    componente header

--- 01-fundamentos-react/src/components/Header.jsx
+++ 01-fundamentos-react/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
-
-
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
     return (
-        <strong className={styles.header}>Header Page</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo do ignite" />
+        </header>
     );
 }
