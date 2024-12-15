45373fc8606e051afebb65e82001ba89782060ec
João Negreiros <joaonegreiros48@gmail.com>
2024-12-15 11:04:01 -0300

    avatar component

--- 01-fundamentos-react/src/components/Post.jsx
+++ 01-fundamentos-react/src/components/Post.jsx
@@ -1,15 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post(){
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img
-                        className={styles.avatar}
-                        src="https://avatars.githubusercontent.com/u/50158723?v=4"
-                    />
+                    <Avatar src="https://avatars.githubusercontent.com/u/50158723?v=4"/>
                     <div className={styles.authorInfo}>
                         <strong>João Negreiros</strong>
                         <span>Web Developer</span>
