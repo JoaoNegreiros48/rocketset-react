0a69a063895c6d6b269777cf6a96eb2a6af9327c
João Negreiros <joaonegreiros48@gmail.com>
2024-12-16 09:01:48 -0300

    Adicionando content/author iterado no post

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -17,7 +17,7 @@ const posts = [
       {type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type:'link', content: 'jane.design/doctorcare'},
     ],
-    publishedAt: new Date('2024-12-05 20:00:00')
+    publishedAt: new Date('2024-12-16 08:30:00')
   },
   {
     id: 2,
