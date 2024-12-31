91bd21ea0208d2202e3a0be6e8de3a6171523a49
João Negreiros <joaonegreiros48@gmail.com>
2024-12-31 09:21:35 -0300

    Correções

--- 01-fundamentos-react-ts/src/components/Avatar.tsx
+++ 01-fundamentos-react-ts/src/components/Avatar.tsx
@@ -1,15 +1,21 @@
+import { ImgHTMLAttributes } from 'react'
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean; // ?: significa que é opcional
-    src: string;
 }
 
-export function Avatar({ hasBorder = true, src }: AvatarProps){
+export function Avatar({ hasBorder = true, ...props }: AvatarProps){
     return(
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-            src={src} 
+            {...props} 
         />
     );
 }
+
+
+// # Esse codigo faz o seguinte
+// 1. Extende o componente de img do react em um interface
+// 2. Pega todos os elementos que vem além do hasborder para o component com o rest operator(...)
+// 3. Passa para a img usando spread operator(...) para passar todas de um só vez para img 
