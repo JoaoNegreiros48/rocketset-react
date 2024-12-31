efae665ac28c0e6dbb258580f457c67b38e44af4
João Negreiros <joaonegreiros48@gmail.com>
2024-12-31 09:03:08 -0300

    Adicionando projeto ts

---
+++ 01-fundamentos-react-ts/src/App.tsx
@@ -0,0 +1,64 @@
+import {Header} from './components/Header'
+import {Post} from './components/Post'
+import {Sidebar} from './components/Sidebar'
+import './global.css'
+import styles from './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/50158723?v=4',
+      name: 'João Negreiros',
+      role: 'Web Developer'
+    },
+    content: [
+      {type:'paragraph', content: 'Fala galeraa 👋'},
+      {type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type:'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-12-16 08:30:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Lks-lukas.png',
+      name: 'Lukinhas',
+      role: 'Usa C# pra ganhar dinheiro'
+    },
+    content: [
+      {type:'paragraph', content: 'Fala galeraa 👋'},
+      {type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type:'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-12-08 20:00:00')
+  },
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {
+            posts.map(post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ 01-fundamentos-react-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.pexels.com/photos/29704575/pexels-photo-29704575.jpeg?auto=compress&cs=tinysrgb&w=500&q=50&dpr=1" 
+            />
+        
+            <div className={styles.profile}>
+                <Avatar src="https://avatars.githubusercontent.com/u/50158723?v=4"/>
+
+                <strong>João Negreiros</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
