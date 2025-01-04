7f4eb3c3963e82fe94955094c86ce6630fdc918e
João Negreiros <joaonegreiros48@gmail.com>
2025-01-04 15:30:44 -0300

    formatando data

--- 02-ignite-timer/src/pages/History/Index.tsx
+++ 02-ignite-timer/src/pages/History/Index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './Style'
 import { CyclesContenxt } from '../../contexts/CyclesContext'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 export function History() {
   const { cycles } = useContext(CyclesContenxt)
@@ -25,7 +27,11 @@ export function History() {
                   <tr key={cycle.id}>
                     <td>{cycle.task}</td>
                     <td>{cycle.minutesAmount} minutos</td>
-                    <td>{cycle.startDate.toISOString()}</td>
+                    <td>{formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                    </td>
                     <td>
                       {cycle.finishedDate && (
                         <Status statusColor="green">Concluído</Status>
