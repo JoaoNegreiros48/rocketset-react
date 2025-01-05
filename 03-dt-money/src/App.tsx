02f736d769eda6c5539f6c836da51f78b1e4ed8e
João Negreiros <joaonegreiros48@gmail.com>
2025-01-05 10:18:45 -0300

    configurações iniciais

---
+++ 03-dt-money/src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobvalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobvalStyle />
+
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
