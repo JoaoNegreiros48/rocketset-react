31ef6e9d516034db88da6e4bd1dcf962fc5e9e61
João Negreiros <joaonegreiros48@gmail.com>
2025-01-05 11:05:37 -0300

    table styled

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,33 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>05/12/2025</td>
+                        </tr>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td><PriceHighLight variant="outcome">- R$ 12.000,00</PriceHighLight></td>
+                            <td>Venda</td>
+                            <td>05/12/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
