7ddbcc66899d3e0ee7467a8b22f67000869221da
João Negreiros <joaonegreiros48@gmail.com>
2025-02-22 15:28:12 -0300

    Cabeçalho

--- pizza-shop-test/src/App.tsx
+++ pizza-shop-test/src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
+import './global.css'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
