6ba6c8373a0d089e440c1f48f85fdda61810e026
João Negreiros <joaonegreiros48@gmail.com>
2025-02-26 20:43:07 -0300

    filtros

---
+++ pizza-shop-test/src/pages/app/orders/order-table-filters.tsx
@@ -0,0 +1,44 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Search, X } from 'lucide-react'
+
+export function OrderTablesFilter() {
+  return (
+    <form className="flex items-center gap-2">
+      <span className="text-sm font-semibold">Filtros:</span>
+      <Input placeholder="ID do pedido" className="h-8 w-auto" />
+      <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+      <Select defaultValue="all">
+        <SelectTrigger className="h-8 w-[180px]">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">Todos</SelectItem>
+          <SelectItem value="pending">Pendente</SelectItem>
+          <SelectItem value="canceled">Cancelado</SelectItem>
+          <SelectItem value="processing">Em preparo</SelectItem>
+          <SelectItem value="delivering">Em entrega</SelectItem>
+          <SelectItem value="delivered">Entregue</SelectItem>
+
+        </SelectContent>
+      </Select>
+
+      <Button type="submit" className="h-8 mr-2" variant="secondary" size="xs">
+        <Search />
+        Filtrar
+      </Button>
+
+      <Button type="button" className="h-8 mr-2" variant="secondary" size="xs">
+        <X />
+        Limpar
+      </Button>
+    </form>
+  )
+}
